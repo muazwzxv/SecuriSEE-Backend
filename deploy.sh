c0c255af85e90ef2af997eaa29b318747d7ea65d
Cilantroz <muazwazir@gmail.com>
2021-09-12 23:14:59 +0800

    script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ mv Oracle-Hackathon-BE /root/go
 mv config.yml /root/go
 
 # Restart the application service
-systemctl restart Go-Backend.service
-systemctl enable Go-Backend.service
+#systemctl restart Go-Backend.service
+#systemctl enable Go-Backend.service
+systemctl daemon-reload
 
 # Reload nginx
 nginx -t && nginx -s reload
