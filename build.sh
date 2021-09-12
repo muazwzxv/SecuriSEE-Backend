4390d96c525a1e5cbeb5e2b32764ee593bb509d6
Cilantroz <muazwazir@gmail.com>
2021-09-12 22:53:41 +0800

    yeet

--- build.sh
+++ build.sh
@@ -8,4 +8,7 @@ GOOS=linux GOARCH=amd64 go build
 # Move to /root/go
 mv Oracle-Hackathon-BE /root/go
 
+# Restart the application service
+systemctl restart Go-Backend.service
+systemctl enable Go-Backend.service
 
