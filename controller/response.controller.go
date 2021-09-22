a79f0e038a27f2019c0b2a05116d55c44a52e3bb
Dolpheyn <farissufyan99@gmail.com>
2021-09-22 09:23:29 +0800

    Create funcs for frequently used responses
    All responses now has json of type { success: bool, message: string, data: error | interface{} }

--- controller/response.controller.go
+++ controller/response.controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+  "io"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,12 +12,42 @@ func response(ctx *fiber.Ctx, status int, msg fiber.Map) error {
 		return ctx.Status(status).JSON(msg)
 }
 
+// Return `controller.Error()` with `StatusForbidden`(403)
+func Forbidden(ctx *fiber.Ctx, msg string, err error) error {
+  return Error(ctx, msg, err, http.StatusForbidden)
+}
+
+// Return `controller.Error()` with `StatusConflict`(409)
+func Conflict(ctx *fiber.Ctx, msg string, err error) error {
+  return Error(ctx, msg, err, http.StatusConflict)
+}
+
+// Return `controller.Error()` with `StatusNotFound`(404)
+func NotFound(ctx *fiber.Ctx, msg string, err error) error {
+  return Error(ctx, msg, err, http.StatusNotFound)
+}
+
+// Return `controller.Error()` with `StatusBadRequest`(400)
+func BadRequest(ctx *fiber.Ctx, msg string, err error) error {
+  return Error(ctx, msg, err, http.StatusBadRequest)
+}
+
 func Error(ctx *fiber.Ctx, msg string, e error, status int) error {
   return response(ctx, status, fiber.Map{
     "success": false,
     "message": msg,
     "data": e,
   })
+}
+
+// Return `controller.Success()` with `StatusCreated`(201) status code
+func Created(ctx *fiber.Ctx, msg string, data interface{}) error {
+  return Success(ctx, msg, data, http.StatusCreated)
+}
+
+// Return `controller.Success()` with `StatusOk`(20o) status code
+func Ok(ctx *fiber.Ctx, msg string, data interface{}) error {
+  return Success(ctx, msg, data, http.StatusOK)
 }
 
 func Success(ctx *fiber.Ctx, msg string, data interface{}, status int) error {
